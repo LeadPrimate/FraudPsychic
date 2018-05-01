0684c72827ce4f9ccc1e1dc6daa1daca8456a6c8
Ryan Norman <ren38@txstate.edu>
2018-04-30 19:28:33 -0500

    Making a final few tweaks and removing excess files

--- Matlab Files/boundary.m
+++ Matlab Files/boundary.m
@@ -1,4 +1,4 @@
-function plotDecisionBoundary(theta, X, y)
+function boundary(theta, X, y)
 %PLOTDECISIONBOUNDARY Plots the data points X and y into a new figure with
 %the decision boundary defined by theta
 %   PLOTDECISIONBOUNDARY(theta, X,y) plots the data points with + for the 
